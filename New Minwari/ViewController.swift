619db7c707f72bc6749d6e9996395279c695eb56
Namikey <developer.ios.kazuaki@gmail.com>
2023-01-30 16:47:19 +0900

    Initial Commit

---
+++ New Minwari/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  New Minwari
+//
+//  Created by 並木一晃 on 2023/01/30.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
